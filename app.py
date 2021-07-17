ed47cf9d1c0883cce344b7d1e7ef7af83fe2c9a5
Divyam Sharma <divyamsharma03@gmail.com>
2021-07-17 14:42:38 +0530

    final

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import cv2
 #import tensorflow as tf
 #from keras.preprocessing import image
 from werkzeug.utils import secure_filename
@@ -15,7 +14,7 @@ html_temp = """
    <div class="" style="background-color:salmon;" >
    <div class="clearfix">           
    <div class="col-md-12">
-   <center><p style="font-size:40px;color:white;margin-top:10px;">Divyam Sharma - PIET18CS049</p></center> 
+   <center><p style="font-size:40px;color:white;margin-top:10px;">Divyam Sharma - PIET18CS050</p></center> 
    <center><p style="font-size:30px;color:white;margin-top:10px;">Digital Image Processing End-Term Examination</p></center> 
    </div>
    </div>
@@ -27,47 +26,20 @@ st.title("""
         Collor Palette
          """
          )
+R = 0
+G = 0
+B = st.slider('B', min_value=0, max_value=255, step=1)
 
-
-img1= st.file_uploader("Please upload image 1", type=("jpg", "png"))
-img2= st.file_uploader("Please upload image 2", type=("jpg", "png"))
-option = st.selectbox('Choose Appropriate option',('Logical XOR', 'Logical AND'))
-
-if img1 is None:
-  st.text("Please upload an Image 1")
-else:
-  file_bytes = np.asarray(bytearray(img1.read()), dtype=np.uint8)
-  image = cv2.imdecode(file_bytes, 1)
-  st.image(img1,caption='Uploaded Image 1', use_column_width=True)
-
-if img2 is None:
-  st.text("Please upload an Image 2")
-else:
-  file_bytes = np.asarray(bytearray(img2.read()), dtype=np.uint8)
-  image = cv2.imdecode(file_bytes, 1)
-  st.image(img2,caption='Uploaded Image 2', use_column_width=True)
-
-
-
-st.write('You selected:', option)
-
-
+import cv2
 from  PIL import Image, ImageOps
 def import_and_predict():
-  file_bytes1 = np.asarray(bytearray(img1.read()), dtype=np.uint8)
-  opencv_image1 = cv2.imdecode(file_bytes1, 1)
-  imga = cv2.resize(opencv_image1,(300,300))
-  file_bytes2 = np.asarray(bytearray(img2.read()), dtype=np.uint8)
-  opencv_image2 = cv2.imdecode(file_bytes2, 1)
-  imgb = cv2.resize(opencv_image2,(300,300))
-  if option == "Logical XOR":
-     result = cv2.bitwise_xor(imga,imgb)
-  else:
-     result = cv2.bitwise_and(imga,imgb)
-  st.image(result,  use_column_width=True)
-  return 0 
+  image_data = np.zeros((100,100,3), np.uint8)
+	   
+  image_data[:] = [R,G,B]
+  st.image(image_data, use_column_width=True)
+  return 0
     
-if st.button("Click To Perform Operation"):
+if st.button("Change Color"):
   result=import_and_predict()
   
 if st.button("About"):
